2913da197b18b64ac3c2646a4b8ed144a1ebf371
daivik1515 <daivik.15.saharan@gmail.com>
2024-11-07 02:17:31 -0500

    worked on order intefacee

---
+++ src/app/views/previous-order/previous-order.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-previous-order',
+  standalone: true,
+  imports: [],
+  templateUrl: './previous-order.component.html',
+  styleUrl: './previous-order.component.css'
+})
+export class PreviousOrderComponent {
+
+}
---
+++ src/app/views/products/products.component.ts
@@ -0,0 +1,26 @@
+import { Component,inject,OnInit, signal } from '@angular/core';
+import { EcommerceServiceService } from '../../service/ecommerce-service.service';
+import { APIResponse, ProductData } from '../../model/Product';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent implements OnInit{
+  ecomService=inject(EcommerceServiceService);
+  data=signal<ProductData[]>([]);
+ ngOnInit(): void {
+  this.displayProducts()
+ }
+
+  displayProducts()
+  {
+    console.log("sdasd");
+    this.ecomService.getProducts().subscribe((res:APIResponse)=>{
+      this.data.set(res.data);
+    })
+  }
+}
