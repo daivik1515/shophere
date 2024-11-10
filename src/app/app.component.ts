818168b6fe3c4b86c43d5439a51eef009db1a7c1
daivik1515 <daivik.15.saharan@gmail.com>
2024-11-10 02:48:34 -0500

    Added the register and login pop-ups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'shophere';
+  showRegister:boolean=false;
+  showLogin:boolean=false;
+  openSignUp(){
+    if(this.showLogin==true)
+      this.showLogin=false;
+    this.showRegister=true;
+  }
+  openLogIn()
+  {
+    if(this.showRegister==true)
+      this.showRegister=false;
+    this.showLogin=true;
+  }
+ 
 }
